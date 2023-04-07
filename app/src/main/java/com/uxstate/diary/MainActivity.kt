9383e02198dcefc7d2999a0d3f4eb6f1f9607a14
Tonnie <vontonnie@gmail.com>
2023-04-07 12:40:52 +0300

    Install SplashScreen

--- app/src/main/java/com/uxstate/diary/MainActivity.kt
+++ app/src/main/java/com/uxstate/diary/MainActivity.kt
@@ -10,34 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.uxstate.diary.ui.theme.DiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //from Splash API
+        installSplashScreen()
         setContent {
             DiaryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DiaryTheme {
-        Greeting("Android")
     }
 }
