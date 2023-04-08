0490364b737bd399346b2f7a94ed3f8091389ead
Tonnie <vontonnie@gmail.com>
2023-04-08 08:19:12 +0300

    Add Client Id constant

--- app/src/main/java/com/uxstate/diary/uti/Constants.kt
+++ app/src/main/java/com/uxstate/diary/uti/Constants.kt
@@ -3,4 +3,5 @@ package com.uxstate.diary.uti
 object Constants {
 
     const val APP_ID = "mydiary-htqya"
+    const val CLIENT_ID = "126637157530-kv1flf0gcslm8fvs52jnr53e41n0mb6j.apps.googleusercontent.com"
 }
