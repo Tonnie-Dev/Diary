15dba64a018808b6b126ffbac701f571204607f3
Tonnie <vontonnie@gmail.com>
2023-04-08 13:23:10 +0300

    Add HomeScreen Composable

---
+++ app/src/main/java/com/uxstate/diary/presentation/screens/HomeScreen/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.uxstate.diary.presentation.screens.HomeScreen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun HomeScreen() {
+
+}
