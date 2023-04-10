dd4f6d2686aedc43e489a922b7169b7f5fdcb5f3
Tonnie <vontonnie@gmail.com>
2023-04-10 18:13:58 +0300

    Add DisplayAlertDialogComposable

--- app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/HomeScreen.kt
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/HomeScreen.kt
@@ -2,21 +2,28 @@ package com.uxstate.diary.presentation.screens.home_screen
 
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.diary.presentation.screens.destinations.AuthenticationScreenDestination
 import com.uxstate.diary.presentation.screens.home_screen.components.DiaryNavigationDrawer
+import com.uxstate.diary.presentation.screens.home_screen.components.DisplayAlertDialog
 import com.uxstate.diary.presentation.screens.home_screen.components.HomeScaffold
+import com.uxstate.diary.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
+import com.uxstate.diary.R
 
 @Destination
 @Composable
 fun HomeScreen(navigator: DestinationsNavigator) {
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    var isSignOutDialogOpen by remember { mutableStateOf(false) }
 
-    //drawerState.open is suspend
+    //drawerState.open/signIn/signOut is suspend
     val scope = rememberCoroutineScope()
 
     DiaryNavigationDrawer(
@@ -25,15 +32,37 @@ fun HomeScreen(navigator: DestinationsNavigator) {
                 HomeScaffold(
                         navigator = navigator,
                         onMenuClicked = {
-                                scope.launch {
+                            scope.launch {
 
-                                        drawerState.open()
-                                }
+                                drawerState.open()
+                            }
 
                         })
             },
-            onSignOutClicked = { /*TODO*/ }
+            onSignOutClicked = { isSignOutDialogOpen = true }
     )
 
+    //Park Alert Dialog to be triggered on isSignOutDialogOpen
+
+    DisplayAlertDialog(
+            title = stringResource(id = R.string.sign_out),
+            message = stringResource(R.string.sign_out_confirmation_text),
+            isDialogOpen = isSignOutDialogOpen,
+            onCloseDialog = { isSignOutDialogOpen = false },
+            onDialogConfirmed = {
+                val user = App.Companion.create(APP_ID).currentUser
+                if (user != null) {
+                    scope.launch(IO) {
+
+                        user.logOut()
+                    }
+
+                }
+                isSignOutDialogOpen = false
+
+                //pop out our home screen for security
+                navigator.popBackStack()
+                navigator.navigate(AuthenticationScreenDestination)
+            })
 
 }
---
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/DisplayAlertDialog.kt
@@ -0,0 +1,71 @@
+package com.uxstate.diary.presentation.screens.home_screen.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.uxstate.diary.R
+import com.uxstate.diary.presentation.ui.theme.LocalSpacing
+
+
+@Composable
+fun DisplayAlertDialog(
+    title: String,
+    message: String,
+    isDialogOpen: Boolean,
+    onCloseDialog: () -> Unit,
+    onDialogConfirmed: () -> Unit
+) {
+    val spacing = LocalSpacing.current
+    if (isDialogOpen) {
+
+        AlertDialog(
+
+                title = {
+
+                    Text(
+                            text = title,
+                            style = TextStyle(
+                                    fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                                    fontWeight = FontWeight.Bold
+                            )
+                    )
+                },
+                text = {
+                    Text(
+                            text = message,
+                            style = TextStyle(
+                                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+
+                                    )
+                    )
+                },
+
+                dismissButton = {
+                    OutlinedButton(onClick = onCloseDialog) {
+                        Text(text = stringResource(R.string.no_text))
+                    }
+
+                },
+
+                confirmButton = {
+
+                    OutlinedButton(onClick = {
+                        onDialogConfirmed()
+                        onCloseDialog()
+                    }) {
+
+                        Text(text = stringResource(R.string.yes_text))
+                    }
+                },
+                onDismissRequest = onCloseDialog
+        )
+    }
+
+}
