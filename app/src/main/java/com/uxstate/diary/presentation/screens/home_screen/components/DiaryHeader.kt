b528cdf12f393f8171b31b8dc54d930224baec96
Tonnie <vontonnie@gmail.com>
2023-04-12 08:01:20 +0300

    Refactor DiaryHeader into a separate file

--- app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/DiaryHeader.kt
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/DiaryHeader.kt
@@ -1,18 +1,57 @@
 package com.uxstate.diary.presentation.screens.home_screen.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import com.uxstate.diary.R
 import com.uxstate.diary.domain.model.Mood
+import com.uxstate.diary.presentation.ui.theme.LocalSpacing
 import java.time.Instant
 
 @Composable
-fun DiaryHeader(moodName:String, time:Instant) {
+fun DiaryHeader(moodName: String, time: Instant) {
+
+    val spacing = LocalSpacing.current
+
+    //value of returns the actual enum
+    val mood by remember { mutableStateOf(Mood.valueOf(moodName)) }
+
+    Row(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .background(mood.containerColor)
+                    .padding(horizontal = spacing.spaceMedium, vertical = spacing.spaceSmall),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+    ) {
 
 
-    Row(modifier = Modifier.fillMaxWidth().background(Mood.valueOf(moodName).)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                    modifier = Modifier.size(spacing.spaceMedium),
+                    painter = painterResource(id = mood.icon),
+                    contentDescription = stringResource(R.string.mood_icon)
+            )
 
+            Spacer(modifier = Modifier.width(spacing.spaceSmall))
+            Text(
+                    text = Mood.valueOf(moodName).name,
+                    style = TextStyle(
+                            color = mood.contentColor,
+                            fontSize = MaterialTheme.typography.bodyMedium.fontSize
+                    )
+            )
+        }
     }
 }
--- app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/DiaryHolder.kt
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/DiaryHolder.kt
@@ -2,13 +2,14 @@ package com.uxstate.diary.presentation.screens.home_screen.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import com.uxstate.diary.domain.model.Diary
 import com.uxstate.diary.presentation.ui.theme.LocalElevation
 import com.uxstate.diary.presentation.ui.theme.LocalSpacing
@@ -16,6 +17,7 @@ import com.uxstate.diary.presentation.ui.theme.LocalSpacing
 @Composable
 fun DiaryHolder(diary: Diary, onClickDiary: (objectId: String) -> Unit) {
 
+    val density = LocalDensity.current
     val spacing = LocalSpacing.current
     val elevation = LocalElevation.current
 
@@ -35,15 +37,33 @@ fun DiaryHolder(diary: Diary, onClickDiary: (objectId: String) -> Unit) {
     {
 
         Spacer(modifier = Modifier.width(spacing.spaceMedium))
+
+        //vertical line
         Surface(
                 modifier = Modifier
                         .width(spacing.spaceDoubleDp)
-                        .height(componentHeight),
+                        .height(componentHeight + spacing.spaceMedium),
                 tonalElevation = elevation.level1
         ) {
 
         }
+
         Spacer(modifier = Modifier.width(spacing.spaceMedium))
+
+        //Diary Entry
+
+        Surface(
+                modifier = Modifier
+                        .clip(shape = Shapes().medium)
+                        .onGloballyPositioned {
+                            componentHeight = with(density) { it.size.height.toDp() }
+                        }, tonalElevation = elevation.level1
+        ) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+
+                DiaryHeader(moodName = diary.mood, time = )
+            }
+        }
     }
 
 }
