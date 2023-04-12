e4b2b3baf94602a2e7e59c18b8690498cd6b9a53
Tonnie <vontonnie@gmail.com>
2023-04-12 08:31:08 +0300

    Add RealInstant.toInstant() extension function

---
+++ app/src/main/java/com/uxstate/diary/util/UtilFunctons.kt
@@ -0,0 +1,15 @@
+package com.uxstate.diary.util
+
+import io.realm.kotlin.types.RealmInstant
+import java.time.Instant
+
+fun RealmInstant.toInstant( ):Instant {
+
+    val sec:Long = this.epochSeconds
+    val nano:Int = this.nanosecondsOfSecond
+
+    return if(sec>=0)
+        Instant.ofEpochSecond(sec, nano.toLong())
+    else
+        Instant.ofEpochSecond((sec-1), 1_000_000 + nano.toLong())
+}
