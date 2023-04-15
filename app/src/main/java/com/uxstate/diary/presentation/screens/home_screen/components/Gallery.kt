1ea86a135e58308c151cac522473d0c227b3d55e
Tonnie <vontonnie@gmail.com>
2023-04-15 20:05:38 +0300

    Add LastImageOverlay composable file

---
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/Gallery.kt
@@ -0,0 +1,80 @@
+package com.uxstate.diary.presentation.screens.home_screen.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material3.Shapes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.uxstate.diary.R
+import kotlin.math.max
+
+@Composable
+fun Gallery(
+    modifier: Modifier = Modifier,
+    images: List<String>,
+    imageSize: Dp = 40.dp,
+    spaceBetween: Dp = 10.dp,
+    imageShape: CornerBasedShape = Shapes().small
+) {
+
+    val context = LocalContext.current
+
+    BoxWithConstraints {
+
+        //cache and retain value across recompositions
+        val numberOfVisibleImages by remember {
+
+            //Buffer to prevent recomposition
+            derivedStateOf {
+
+                max(
+                        a = 0,
+                        b = this.maxWidth.div(imageSize + spaceBetween)
+                                .toInt()
+                                .minus(1)
+                )
+            }
+        }
+
+
+        val remainingImages by remember {
+            derivedStateOf { images.size - numberOfVisibleImages }
+        }
+
+        Row (horizontalArrangement = Arrangement.spacedBy(spaceBetween)){
+            images.take(numberOfVisibleImages)
+                    .forEach {
+
+                        image ->
+
+                        val painter = rememberAsyncImagePainter(
+                                model = ImageRequest.Builder(context = context)
+                                        .data(image)
+                                        .crossfade(true)
+                                        .build()
+                        )
+
+                        Image(
+                                painter = painter,
+                                contentDescription = stringResource(R.string.gallery_image_text),
+                        modifier = Modifier.clip(imageShape).size(imageSize)
+                        )
+                    }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/components/LastImageOverlay.kt
@@ -0,0 +1,56 @@
+package com.uxstate.diary.presentation.screens.home_screen.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.uxstate.diary.presentation.ui.theme.DiaryTheme
+
+@Composable
+fun LastImageOverlay(
+    imageSize: Dp,
+    remainingImages: Int,
+    imageShape: CornerBasedShape
+) {
+
+
+   Box(contentAlignment = Alignment.Center) {
+
+        Surface(
+                modifier = Modifier
+                        .clip(imageShape)
+                        .size(imageSize), color = MaterialTheme.colorScheme.primaryContainer
+        ) {
+
+        }
+       Text(
+               text = remainingImages.toString(),
+               style = TextStyle(
+                       fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                       fontWeight = FontWeight.Medium,
+                       color = MaterialTheme.colorScheme.onPrimaryContainer
+               )
+       )
+
+    }
+}
+
+@Preview
+@Composable
+fun LastImageOverlayPrev() {
+    DiaryTheme() {
+        LastImageOverlay(imageSize =40.dp, remainingImages = 17, imageShape = Shapes().small)
+    }
+}
