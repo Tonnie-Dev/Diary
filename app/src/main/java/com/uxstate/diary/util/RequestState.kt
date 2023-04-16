2bec51773203dd2560c2c89136afe0ad7a9fb997
Tonnie <vontonnie@gmail.com>
2023-04-16 20:32:14 +0300

    Add RequestState Sealed class

---
+++ app/src/main/java/com/uxstate/diary/util/RequestState.kt
@@ -0,0 +1,9 @@
+package com.uxstate.diary.util
+
+sealed class RequestState<out T> {
+
+    object Idle: RequestState<Nothing>()
+    object Loading:RequestState<Nothing>()
+    data class Success<T>(val data:T):RequestState<T>()
+    data class Error(val error:Throwable):RequestState<Nothing>()
+}
