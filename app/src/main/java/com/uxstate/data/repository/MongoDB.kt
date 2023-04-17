b5fe8db4f456ed1f662b533890ee3a75182eb2e0
Tonnie <vontonnie@gmail.com>
2023-04-17 05:31:52 +0300

    Implement getAllDiaries() inside MongoDB class

--- app/src/main/java/com/uxstate/data/repository/MongoDB.kt
+++ app/src/main/java/com/uxstate/data/repository/MongoDB.kt
@@ -1,19 +1,32 @@
 package com.uxstate.data.repository
 
 import com.uxstate.diary.domain.model.Diary
+import com.uxstate.diary.domain.repository.Diaries
 import com.uxstate.diary.domain.repository.MongoRepository
 import com.uxstate.diary.util.Constants.APP_ID
+import com.uxstate.diary.util.RequestState
+import com.uxstate.diary.util.toInstant
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.log.LogLevel
 import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.sync.SyncConfiguration
+import io.realm.kotlin.query.Sort
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import java.time.ZoneId
 
 object MongoDB : MongoRepository {
     private val app = App.create(APP_ID)
     private val user = app.currentUser
 
     private lateinit var realm: Realm
+
+    init {
+        configureTheRealm()
+    }
+
     override fun configureTheRealm() {
         //super.configureTheRealm()
 
@@ -29,9 +42,39 @@ object MongoDB : MongoRepository {
                         )
                     }
                     .log(LogLevel.ALL)
-                 .build()
+                    .build()
 
             realm = Realm.open(config)
         }
     }
+
+    override fun getAllDiaries(): Flow<Diaries> {
+
+
+       return if (user != null) {
+
+            try {
+                realm.query<Diary>(query = "ownerId == $0", user.id)
+                        .sort(property = "date", sortOrder = Sort.DESCENDING)
+                        .asFlow()
+                        .map { result ->
+                            RequestState.Success(data = result.list.groupBy {
+                                it.date.toInstant()
+                                        .atZone(ZoneId.systemDefault())
+                                        .toLocalDate()
+                            })
+                        }
+
+            } catch (e: Exception) {
+
+                flow { emit(RequestState.Error(e)) }
+            }
+
+
+        } else {
+            flow { emit(RequestState.Error(UserNotAuthenticatedException())) }
+        }
+    }
 }
+
+private class UserNotAuthenticatedException : Exception("User is not Logged In")
--- app/src/main/java/com/uxstate/diary/domain/repository/MongoRepository.kt
+++ app/src/main/java/com/uxstate/diary/domain/repository/MongoRepository.kt
@@ -1,8 +1,13 @@
 package com.uxstate.diary.domain.repository
 
+import com.uxstate.diary.domain.model.Diary
+import com.uxstate.diary.util.RequestState
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+typealias Diaries = RequestState<Map<LocalDate, List<Diary>>>
 interface MongoRepository    {
 
-    fun configureTheRealm() {
-
-    }
+    fun configureTheRealm()
+    fun getAllDiaries(): Flow<Diaries>
 }
