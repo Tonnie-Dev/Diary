ba777b4b2f1b9fe1dcc6dc24158cee5404e41810
Tonnie <vontonnie@gmail.com>
2023-04-17 19:34:47 +0300

    Add HomeViewModel class

---
+++ app/src/main/java/com/uxstate/diary/presentation/screens/home_screen/HomeViewModel.kt
@@ -0,0 +1,37 @@
+package com.uxstate.diary.presentation.screens.home_screen
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.uxstate.data.repository.MongoDB
+import com.uxstate.diary.domain.repository.Diaries
+import com.uxstate.diary.util.RequestState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+   val  diaries = mutableStateOf<Diaries>(RequestState.Idle)
+
+   init {
+       observeAllDiaries()
+   }
+
+    private fun observeAllDiaries() {
+        viewModelScope.launch {
+
+
+            MongoDB.getAllDiaries().collect{
+
+                result ->
+
+                diaries.value = result
+            }
+        }
+    }
+}
