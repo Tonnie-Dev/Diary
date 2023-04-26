cbfc782856221760e920558341cd1432878d3470
Tonnie <vontonnie@gmail.com>
2023-04-26 15:01:52 +0300

    Update Libs

--- app/src/main/java/com/uxstate/diary/presentation/screens/write_screen/WriteViewModel.kt
+++ app/src/main/java/com/uxstate/diary/presentation/screens/write_screen/WriteViewModel.kt
@@ -1,15 +1,19 @@
 package com.uxstate.diary.presentation.screens.write_screen
 
-import androidx.compose.runtime.collectAsState
+
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.uxstate.diary.data.repository.MongoDB
 import com.uxstate.diary.presentation.screens.navArgs
 import com.uxstate.diary.presentation.screens.write_screen.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.mongodb.kbson.ObjectId
+
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,15 +22,24 @@ class WriteViewModel @Inject constructor(handle: SavedStateHandle) : ViewModel()
     private val _uiState = MutableStateFlow(UiState())
     val uiState = _uiState.asStateFlow()
 
+    private val diaryId = handle.navArgs<WriteScreenNavArgs>().id
 
-    init {
-        val diaryId = handle.navArgs<WriteScreenNavArgs>().id
-        _uiState.update {
 
-            it.copy(selectedDiaryId = diaryId)
+    private fun fetchSelectedDiary() {
+
+
+        if (diaryId != null) {
+
+
+            viewModelScope.launch(IO) {
+
+
+                //val diary =
+                   // MongoDB.getSelectedDiary(diaryId = ObjectId.Companion.from(_uiState.value.selectedDiaryId))
+            }
         }
-    }
 
+    }
 
 
 }
