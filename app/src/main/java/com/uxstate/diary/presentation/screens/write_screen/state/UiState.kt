011be7e4006d793625db61512f5c4f91979c1477
Tonnie <vontonnie@gmail.com>
2023-04-30 13:52:18 +0300

    Add updatedDateTime in the UiDate data class

--- app/src/main/java/com/uxstate/diary/presentation/screens/write_screen/state/UiState.kt
+++ app/src/main/java/com/uxstate/diary/presentation/screens/write_screen/state/UiState.kt
@@ -2,11 +2,13 @@ package com.uxstate.diary.presentation.screens.write_screen.state
 
 import com.uxstate.diary.domain.model.Diary
 import com.uxstate.diary.domain.model.Mood
+import io.realm.kotlin.types.RealmInstant
 
 data class UiState(
     val selectedDiaryId: String? = null,
     val selectedDiary: Diary? = null,
     val title: String = "",
     val description: String = "",
-    val mood: Mood = Mood.NEUTRAL
+    val mood: Mood = Mood.NEUTRAL,
+    val updatedDateTime:RealmInstant? = null
 )
