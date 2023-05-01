21abac584fd1db2cf2754ee9f1c485b3bf9ccc2c
Tonnie <vontonnie@gmail.com>
2023-05-01 21:11:27 +0300

    Add GalleyState class functions

--- app/src/main/java/com/uxstate/diary/domain/model/GalleryState.kt
+++ app/src/main/java/com/uxstate/diary/domain/model/GalleryState.kt
@@ -1,8 +1,26 @@
 package com.uxstate.diary.domain.model
 
 import android.net.Uri
+import androidx.compose.runtime.mutableStateListOf
 
 class GalleryState {
+
+    val images = mutableStateListOf<GalleryImage>()
+    val imagesToBeDeleted = mutableStateListOf<GalleryImage>()
+
+    fun addImage(galleryImage: GalleryImage){
+
+        images.add(galleryImage)
+    }
+
+    fun removeImage(galleryImage: GalleryImage){
+        images.remove(galleryImage)
+        imagesToBeDeleted.add(galleryImage)
+    }
+
+    fun clearImagesToBeDeleted(){
+        imagesToBeDeleted.clear()
+    }
 }
 
 
