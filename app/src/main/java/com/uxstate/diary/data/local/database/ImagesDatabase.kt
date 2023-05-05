9bb1f6693ec99e57ff0e495d4fc10c04a718f21a
Tonnie <vontonnie@gmail.com>
2023-05-05 08:07:53 +0300

    Add DatabaseModule interface

--- app/src/main/java/com/uxstate/diary/data/local/database/ImagesDatabase.kt
+++ app/src/main/java/com/uxstate/diary/data/local/database/ImagesDatabase.kt
@@ -1,11 +1,19 @@
 package com.uxstate.diary.data.local.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.uxstate.diary.data.local.dao.ImageToUploadDao
+import com.uxstate.diary.data.local.database.ImagesDatabase.Companion.DATABASE_VERSION
 import kotlin.math.exp
 
-@Database(entities = [ImagesDatabase::class], version = 1, exportSchema = false)
-abstract class ImagesDatabase {
+@Database(entities = [ImagesDatabase::class], version = DATABASE_VERSION, exportSchema = false)
+abstract class ImagesDatabase:RoomDatabase(){
 
     abstract val imageToUploadDao:ImageToUploadDao
+
+    companion object {
+        const val DATABASE_NAME = "images_db"
+        const val DATABASE_VERSION = 1
+
+    }
 }
---
+++ app/src/main/java/com/uxstate/diary/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.uxstate.diary.di
+
+import android.content.Context
+import androidx.room.Room
+import com.uxstate.diary.data.local.database.ImagesDatabase
+import com.uxstate.diary.data.local.database.ImagesDatabase.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DatabaseModule {
+
+    companion object{
+        @Provides
+        @Singleton
+        fun provideImagesDatabase(@ApplicationContext context: Context): ImagesDatabase {
+
+            return Room.databaseBuilder(context, ImagesDatabase::class.java, DATABASE_NAME)
+                    .build()
+        }
+
+    }
+
+}
