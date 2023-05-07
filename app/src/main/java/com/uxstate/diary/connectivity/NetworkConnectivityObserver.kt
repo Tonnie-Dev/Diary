2357138929c4af6f306652a8dcdf12b52cbf8a28
Tonnie <vontonnie@gmail.com>
2023-05-07 17:13:01 +0300

    Override observe() inside NetworkConnectivityObserver class

--- app/src/main/java/com/uxstate/diary/connectivity/NetworkConnectivityObserver.kt
+++ app/src/main/java/com/uxstate/diary/connectivity/NetworkConnectivityObserver.kt
@@ -1,10 +1,53 @@
 package com.uxstate.diary.connectivity
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
+import androidx.annotation.RequiresApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
-class NetworkConnectivityObserver(context:Context): ConnectivityObserver {
+class NetworkConnectivityObserver(context: Context) : ConnectivityObserver {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
     override fun observe(): Flow<ConnectivityObserver.Status> {
-        TODO("Not yet implemented")
+
+        return callbackFlow {
+        //anonymously introducing a call back
+        val callback = object : ConnectivityManager.NetworkCallback() {
+
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                launch { send(ConnectivityObserver.Status.AVAILABLE) }
+            }
+
+            override fun onLosing(network: Network, maxMsToLive: Int) {
+                super.onLosing(network, maxMsToLive)
+                launch { send(ConnectivityObserver.Status.LOSING) }
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                launch { send(ConnectivityObserver.Status.LOST) }
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                launch { send(ConnectivityObserver.Status.UNAVAILABLE) }
+            }
+        }
+
+        connectivityManager.registerDefaultNetworkCallback(callback)
+
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+
+    }.distinctUntilChanged()
     }
 }
