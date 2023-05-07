be3e03f9c8854df689f78c64f7f3b82e70b989c3
Tonnie <vontonnie@gmail.com>
2023-05-07 14:03:41 +0300

    Add ImageToDeleteDao queries

--- app/src/main/java/com/uxstate/diary/data/local/dao/ImageToDeleteDao.kt
+++ app/src/main/java/com/uxstate/diary/data/local/dao/ImageToDeleteDao.kt
@@ -1,7 +1,19 @@
 package com.uxstate.diary.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.uxstate.diary.data.local.entities.ImageToDelete
 
 @Dao
 interface ImageToDeleteDao {
+
+    @Query("SELECT * FROM image_to_delete_table ORDER BY id ASC")
+    suspend fun getAllImages():List<ImageToDelete>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addImageToDelete(imageToDelete: ImageToDelete)
+
+    @Query("DELETE FROM image_to_delete_table WHERE id=:imageId")
+    suspend fun cleanUpImage(imageId:Int)
 }
