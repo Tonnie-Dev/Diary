ecc71ae6071e219dd67d8e52a4ea802914e6f440
Tonnie <vontonnie@gmail.com>
2023-05-07 17:35:43 +0300

    Add provideConnectivityObserver() di function

--- app/src/main/java/com/uxstate/diary/connectivity/ConnectivityObserverImpl.kt
+++ app/src/main/java/com/uxstate/diary/connectivity/ConnectivityObserverImpl.kt
@@ -3,50 +3,51 @@ package com.uxstate.diary.connectivity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NetworkConnectivityObserver(context: Context) : ConnectivityObserver {
+class ConnectivityObserverImpl @Inject constructor(context: Context) : ConnectivityObserver {
     private val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     override fun observe(): Flow<ConnectivityObserver.Status> {
 
         return callbackFlow {
-        //anonymously introducing a call back
-        val callback = object : ConnectivityManager.NetworkCallback() {
+            //anonymously introducing a call back
+            val callback = object : ConnectivityManager.NetworkCallback() {
 
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                launch { send(ConnectivityObserver.Status.AVAILABLE) }
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    launch { send(ConnectivityObserver.Status.AVAILABLE) }
+                }
+
+                override fun onLosing(network: Network, maxMsToLive: Int) {
+                    super.onLosing(network, maxMsToLive)
+                    launch { send(ConnectivityObserver.Status.LOSING) }
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    launch { send(ConnectivityObserver.Status.LOST) }
+                }
+
+                override fun onUnavailable() {
+                    super.onUnavailable()
+                    launch { send(ConnectivityObserver.Status.UNAVAILABLE) }
+                }
             }
 
-            override fun onLosing(network: Network, maxMsToLive: Int) {
-                super.onLosing(network, maxMsToLive)
-                launch { send(ConnectivityObserver.Status.LOSING) }
-            }
-
-            override fun onLost(network: Network) {
-                super.onLost(network)
-                launch { send(ConnectivityObserver.Status.LOST) }
-            }
-
-            override fun onUnavailable() {
-                super.onUnavailable()
-                launch { send(ConnectivityObserver.Status.UNAVAILABLE) }
-            }
-        }
-
-        connectivityManager.registerDefaultNetworkCallback(callback)
+            connectivityManager.registerDefaultNetworkCallback(callback)
 
             //unregister callback - triggered when flow is cancelled
-        awaitClose {
-            connectivityManager.unregisterNetworkCallback(callback)
-        }
-//return distinct flow enum values - ignores consecutive save enum values
-    }.distinctUntilChanged()
+            awaitClose {
+                connectivityManager.unregisterNetworkCallback(callback)
+            }
+            //return distinct flow enum values - ignores consecutive save enum values
+        }.distinctUntilChanged()
     }
 }
--- app/src/main/java/com/uxstate/diary/di/NetworkModule.kt
+++ app/src/main/java/com/uxstate/diary/di/NetworkModule.kt
@@ -1,10 +1,9 @@
 package com.uxstate.diary.di
 
 import com.uxstate.diary.connectivity.ConnectivityObserver
-import com.uxstate.diary.connectivity.NetworkConnectivityObserver
+import com.uxstate.diary.connectivity.ConnectivityObserverImpl
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -12,8 +11,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class NetworkModule {
-
+    //ConnectivityObserverImpl
     @Binds
     @Singleton
-    fun providesNetworkConnectivityObserver(connectivityObserver: ConnectivityObserver) = NetworkConnectivityObserver
+    abstract fun provideConnectivityObserver(
+        connectivityObserverImpl: ConnectivityObserverImpl
+    ): ConnectivityObserver
 }
