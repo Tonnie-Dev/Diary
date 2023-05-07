f5f601e5333e44332fa332758552088628d14166
Tonnie <vontonnie@gmail.com>
2023-05-07 16:25:36 +0300

    Add CoonnectivityObserver onbserve() interface function

--- app/src/main/java/com/uxstate/diary/connectivity/ConnectivityObserver.kt
+++ app/src/main/java/com/uxstate/diary/connectivity/ConnectivityObserver.kt
@@ -1,4 +1,15 @@
 package com.uxstate.diary.connectivity
 
 interface ConnectivityObserver {
+
+    fun onbserve():Flow<Status>
+
+    enum class Staus(){
+
+        AVAILABLE,
+        UNAVAILABLE,
+        LOSING,
+        LOST
+
+    }
 }
