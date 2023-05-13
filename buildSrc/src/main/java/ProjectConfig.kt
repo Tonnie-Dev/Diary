3aa2c4ad58697891d99671c793bc84c21303cc2d
Tonnie <vontonnie@gmail.com>
2023-05-13 15:35:09 +0300

    Add ui module

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -2,6 +2,6 @@ object ProjectConfig {
 
     const val compileSdk = 33
     const val minSdk = 24
-    const val targetSdk = 22
+    const val targetSdk = 33
     const val kotlinCompilerExtensionVersion = "1.4.3"
 }
