fded0dc09cec03c5b7d1497c44b274c2f7a11625
Tonnie <vontonnie@gmail.com>
2023-05-18 07:34:53 +0300

    Add Res Folder to :feature:auth module

--- feature/auth/src/main/java/com/uxstate/auth/components/SignInButton.kt
+++ feature/auth/src/main/java/com/uxstate/auth/components/SignInButton.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.uxstate.diary.R
+import com.uxstate.auth.R
 import com.uxstate.ui.theme.DiaryTheme
 import com.uxstate.ui.theme.LocalSpacing
 
